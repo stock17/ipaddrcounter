6f3a113f9e1b5cb93eb8fda3a19c7f1278255846
Yury <edenstack17@gmail.com>
2020-04-29 12:55:51 +0400

    add 'add' method in HyperLogLog class

--- src/main/java/ru/yurima/ipaddrcounter/HyperLogLog.java
+++ src/main/java/ru/yurima/ipaddrcounter/HyperLogLog.java
@@ -1,16 +1,24 @@
 package ru.yurima.ipaddrcounter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HyperLogLog {
 
-    private int M; // Number of registers
-    int[] registers;
+    private final int M = 16;                               // Number of registers
+    private final int b = (int)(Math.log(M) / Math.log(2)); // Calculate log2M register bits
+    private final int[] registers = new int[M];
 
-    public HyperLogLog(int m) {
-        this.M = m;
-        this.registers = new int[M];
+    {
+        for (int i = 0; i < M-1; i++) {
+            registers[i] = 0;
+        }
     }
 
-    public HyperLogLog() {
-        this(32);
+    public void add(int hashcode){
+        int registerNumber = hashcode >>> (16 - b);         //get b first bits
+        int body = (0xF000 | hashcode) - 0xF000;            //get rest of the bits
+        int mostLeftBit = 16 - Integer.numberOfLeadingZeros(body);
+        registers[registerNumber] = Math.max(registers[registerNumber], mostLeftBit);
     }
 }
